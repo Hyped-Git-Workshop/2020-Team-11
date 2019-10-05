03e69027181dca8b9587143c7a74cc8b62461377
Mohammed <zayrex@DESKTOP-H1BBNJ7.localdomain>
2019-10-05 14:55:49 +0100

    Change the pressure

--- pod.cpp
+++ pod.cpp
@@ -27,4 +27,9 @@ double Pod::get_temperature()
 {
   return -69;
 }
+
+double Pod::get_pressure()
+{
+  return 7;
+}
 /// END OF TASKS 3A & 4B ///////////////////////////////////////////////////////
